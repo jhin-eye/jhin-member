9cd1061cfbb039e0a28ed058fe7c85d39b7b1faf
yeonwoo <hyanoos0915@gmail.com>
2024-01-21 23:09:57 +0900

    fix(kakao login): 메소드명 직관적으로 변경, 아웃풋 변경
    - 아웃풋 변경: 토큰 대신 카카오 회원 정보 리턴하도록 변경

--- src/main/java/com/yanoos/member/service/KakaoLoginService.java
+++ src/main/java/com/yanoos/member/service/KakaoLoginService.java
@@ -1,7 +1,5 @@
 package com.yanoos.member.service;
 
-import com.yanoos.global.exception.ApiException;
-import com.yanoos.global.exception.code.TestErrorCode;
 import com.yanoos.global.util.WebClientService;
 import com.yanoos.global.util.dto.KakaoTokenResDTO;
 import com.yanoos.member.controller.dto.KakaoUser;
@@ -24,26 +22,25 @@ public class KakaoLoginService {
     private String KAKAO_API_KEY;
     @Value("${kakao.redirect-uri}")
     private String REDIRECT_URI;
-    public void setting(Model model){
+    @Value("${kakao.client-secret}")
+    private String CLIENT_SECRET;
+    public void setUrlEnvironment(Model model){
         model.addAttribute("clientId", KAKAO_API_KEY);
         model.addAttribute("redirectUri", REDIRECT_URI);
     }
 
-    public KakaoTokenResDTO handleCallBack(Map<String, String> params) {
-        if(params.containsKey("error")){
-            throw new ApiException(TestErrorCode.TEST_ERROR);
-        }
+    public KakaoUser getUserInfoByReceivedAuthorizationCode(Map<String, String> params) {
         String authorizationCode =  params.get("code");
         log.info("authorizationCode = {}", authorizationCode);
 
         //카카오 토큰 요청
-        KakaoTokenResDTO kakaoTokenResDTO = webClientService.requestKakaoToken(authorizationCode).block();
+        KakaoTokenResDTO kakaoTokenResDTO = webClientService.requestKakaoToken(authorizationCode,KAKAO_API_KEY,CLIENT_SECRET,REDIRECT_URI).block();
         log.info("tokenDTO = {}",kakaoTokenResDTO);
 
-        //카카오 토큰 해석
+        //카카오 토큰으로 유저정보 가져오기
         String kakaoAccessToken = kakaoTokenResDTO.getAccess_token();
         KakaoUser kakaoUser = webClientService.requestKakaoUserInfo(kakaoAccessToken).block();
         log.info("kakao user = {}",kakaoUser);
-        return kakaoTokenResDTO;
+        return kakaoUser;
     }
 }
