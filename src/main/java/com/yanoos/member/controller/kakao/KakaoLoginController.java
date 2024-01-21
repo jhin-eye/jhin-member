f4c03f2c5a0fa05d824a6fb3d37f5291c3e6e962
yeonwoo <hyanoos0915@gmail.com>
2024-01-21 23:08:43 +0900

    fix(kakao login): 메소드명 직관적으로 변경

--- src/main/java/com/yanoos/member/controller/kakao/KakaoLoginController.java
+++ src/main/java/com/yanoos/member/controller/kakao/KakaoLoginController.java
@@ -1,11 +1,10 @@
-package com.yanoos.member.controller.login;
+package com.yanoos.member.controller.kakao;
 
 
-import com.yanoos.global.util.dto.KakaoTokenResDTO;
+import com.yanoos.member.controller.dto.KakaoUser;
 import com.yanoos.member.service.KakaoLoginService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -22,14 +21,14 @@ public class KakaoLoginController {
     //카카오로그인 페이지
     @GetMapping("/login")
     public String getKakaoLoginPage(Model model){
-        kakaoLoginService.setting(model);
+        kakaoLoginService.setUrlEnvironment(model);
         return "kakaoLoginForm";
     }
 
-    //인가코드 받아서 카카오토큰 요청
+    //인가코드 받아서 카카오토큰 요청 -> 카카오토큰으로 유저 정보 가져오기
     @GetMapping("/callback")
     @ResponseBody
-    public KakaoTokenResDTO kakaoCallback(@RequestParam Map<String,String> params){
-        return kakaoLoginService.handleCallBack(params);
+    public KakaoUser getUserInfoByReceivedAuthorizationCode(@RequestParam Map<String,String> params){
+        return kakaoLoginService.getUserInfoByReceivedAuthorizationCode(params);
     }
 }
