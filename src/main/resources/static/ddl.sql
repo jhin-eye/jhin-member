80cf2aa4a5af9d02d6344822674c2c46c4ec43dd
PC <yeonwoo_lee3@tmax.co.kr>
2024-07-18 21:09:47 +0900

    chore(db-script): 네이밍 보기 편하게 수정

--- src/main/resources/static/ddl.sql
+++ src/main/resources/static/ddl.sql
@@ -1,33 +1,37 @@
-create table member(
-                       member_id bigserial,
-                       member_email varchar(255) unique not null,
-                       member_nickname varchar(255) unique not null,
-                       primary key (member_id)
+-- 회원 정보를 저장하는 테이블
+CREATE TABLE member (
+                        member_id BIGSERIAL, -- 고유 회원 ID
+                        member_email VARCHAR(255) UNIQUE NOT NULL, -- 고유 이메일 주소
+                        member_nickname VARCHAR(255) UNIQUE NOT NULL, -- 고유 닉네임
+                        PRIMARY KEY (member_id) -- 기본 키 설정
 );
 
-create table oauth_member(
-                           oauth_id bigserial,
-                           member_id bigint not null,
-                           oauth_host varchar(255) not null,
-                           primary key (oauth_id)
+-- OAuth 회원 정보를 저장하는 테이블
+CREATE TABLE member_oauth (
+                              member_oauth_id BIGSERIAL, -- ID
+                              member_id BIGINT NOT NULL, -- 회원 ID
+                              oauth_host VARCHAR(255) NOT NULL, -- OAuth 호스트 정보
+                              PRIMARY KEY (member_oauth_id) -- 기본 키 설정
 );
 
-create table kakao_member
-(
-                           oauth_id bigint,
-                           kakao_id varchar(255) unique not null,
-                           kakao_email varchar(255) unique not null,
-                           kakao_nickname varchar(255) unique not null,
-                           primary key (oauth_id)
+-- 카카오 회원 정보를 저장하는 테이블
+CREATE TABLE member_oauth_kakao (
+                                    member_oauth_kakao_id BIGSERIAL, -- ID
+                                    kakao_id VARCHAR(255) UNIQUE NOT NULL, -- 고유 카카오 ID
+                                    kakao_email VARCHAR(255) UNIQUE NOT NULL, -- 고유 카카오 이메일
+                                    kakao_nickname VARCHAR(255), -- 카카오 닉네임
+                                    PRIMARY KEY (member_oauth_kakao_id) -- 기본 키 설정
 );
 
-alter table oauth_member
-    add constraint fk_oauth_user_member
-        foreign key (member_id)
-            references member (member_id);
+-- 외래 키 설정
+ALTER TABLE member_oauth
+    ADD CONSTRAINT fk_member_oauth_member
+        FOREIGN KEY (member_id)
+            REFERENCES member (member_id);
+
+ALTER TABLE member_oauth_kakao
+    ADD CONSTRAINT fk_member_oauth_kakao_member_oauth
+        FOREIGN KEY (member_oauth_kakao_id)
+            REFERENCES member_oauth (member_oauth_id);
 
 
-alter table kakao_member
-    add constraint fk_kakao_user_oauth_user
-        foreign key (oauth_id)
-            references oauth_member (oauth_id);
