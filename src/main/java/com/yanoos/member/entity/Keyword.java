6520c08c009913b6f899dc56b74b1ce8ccd681f6
PC <yeonwoo_lee3@tmax.co.kr>
2024-07-20 15:37:18 +0900

    refactor: entity 위치 변경

---
+++ src/main/java/com/yanoos/member/entity/Keyword.java
@@ -0,0 +1,32 @@
+package com.yanoos.member.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "keyword")
+public class Keyword {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "keyword_id")
+    private Long keywordId;
+
+    @Column(name = "keyword", unique = true)
+    private String keyword;
+
+    @OneToMany(mappedBy = "keyword", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<MapMemberKeyword> mapMemberKeywords = new ArrayList<>();
+
+    // Getter, Setter
+}
