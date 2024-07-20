1fa3d2342eaef38bbae96ecc878ac09094d59126
PC <yeonwoo_lee3@tmax.co.kr>
2024-07-21 03:44:49 +0900

    feat: keyword 조회 서비스 생성

---
+++ src/main/java/com/yanoos/member/controller/keyword/KeywordController.java
@@ -0,0 +1,56 @@
+package com.yanoos.member.controller.keyword;
+
+import com.yanoos.global.exception.BusinessException;
+import com.yanoos.global.exception.code.MemberErrorCode;
+import com.yanoos.global.util.AuthUtil;
+import com.yanoos.member.controller.dto.PostKeywordIn;
+import com.yanoos.member.business_service.keyword.KeywordBusinessService;
+import com.yanoos.member.controller.dto.PostKeywordOut;
+import com.yanoos.member.entity.MapMemberKeyword;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Slf4j
+@RestController
+@RequestMapping("/api/keyword")
+public class KeywordController {
+    private final AuthUtil authUtil;
+    private final KeywordBusinessService keywordBusinessService;
+
+
+    /**
+     * 계정 키워드 조회
+     *
+     * @return
+     */
+    @GetMapping
+    public List<String> getKeywords(){
+        List<String> mapMemberKeywords=keywordBusinessService.getKeywordsByMemberId(authUtil.getMemberId());
+        return mapMemberKeywords;
+    }
+    /**
+     * 계정에 키워드 등록
+     * @param postKeywordIn
+     * @return
+     */
+    @PostMapping()
+    public PostKeywordOut postKeyword(@RequestBody PostKeywordIn postKeywordIn) {
+        validatePostKeywordIn(postKeywordIn);
+        PostKeywordOut postKeywordOut = keywordBusinessService.postKeyword(postKeywordIn);
+        return postKeywordOut;
+    }
+
+    private void validatePostKeywordIn(PostKeywordIn postKeywordIn) {
+        Long memberId = authUtil.getMemberId();
+        if(!memberId.equals(postKeywordIn.getMemberId())){
+            throw new BusinessException(MemberErrorCode.MEMBER_NOT_FOUND);
+        }
+        log.info("validatePostKeywordIn success!");
+    }
+}
--- src/main/java/com/yanoos/member/repository/KeywordRepository.java
+++
@@ -1,6 +0,0 @@
-package com.yanoos.member.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KeywordRepository extends JpaRepository<KeywordRepository,Long> {
-}
