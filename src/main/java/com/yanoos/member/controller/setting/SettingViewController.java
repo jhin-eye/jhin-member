c7b8ff2923c45e81d89c4b1701e44d362abe3d3b
PC <yeonwoo_lee3@tmax.co.kr>
2024-08-21 23:24:32 +0900

    feat: 신규 로그아웃 작업완료

--- src/main/java/com/yanoos/member/controller/setting/SettingViewController.java
+++ src/main/java/com/yanoos/member/controller/setting/SettingViewController.java
@@ -2,13 +2,20 @@ package com.yanoos.member.controller.setting;
 
 import com.yanoos.global.exception.BusinessException;
 import com.yanoos.global.exception.code.MemberErrorCode;
+import com.yanoos.global.jwt.TokenType;
 import com.yanoos.global.util.AuthUtil;
+import com.yanoos.global.util.WebClientService;
+import com.yanoos.member.business_service.kakao.KakaoLoginService;
 import com.yanoos.member.business_service.keyword.KeywordBusinessService;
 import com.yanoos.member.business_service.member.MemberBusinessService;
 import com.yanoos.member.controller.dto.AsideMenu;
 import com.yanoos.member.controller.member.GenerateTelegramUuidOut;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +23,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,47 +32,79 @@ import java.util.List;
 @Slf4j
 @RequestMapping("/api/view/settings")
 public class SettingViewController {
+    @Value("${kakao.client-id}")
+    private String KAKAO_API_KEY;
+    @Value("${kakao.logout-redirect-uri}")
+    private String KAKAO_LOGOUT_REDIRECT_URI;
+
+
+
     private final AuthUtil authUtil;
     private final KeywordBusinessService keywordBusinessService;
     private final MemberBusinessService memberBusinessService;
+    private final KakaoLoginService kakaoLoginService;
+
+
+    @GetMapping("/logout/kakao")
+    public void kakaoLogout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        // 쿠키 삭제
+        Cookie accessTokenCookie = new Cookie(TokenType.ACCESS.name(), null);
+        accessTokenCookie.setMaxAge(0);
+        accessTokenCookie.setPath("/");
+        response.addCookie(accessTokenCookie);
+
+        Cookie refreshTokenCookie = new Cookie(TokenType.REFRESH.name(), null);
+        refreshTokenCookie.setMaxAge(0);
+        refreshTokenCookie.setPath("/");
+        response.addCookie(refreshTokenCookie);
+
+        // 홈으로 이동
+        // 리다이렉트할 URL 생성
+        String redirectUrl = "https://kauth.kakao.com/oauth/logout?client_id=" + KAKAO_API_KEY + "&logout_redirect_uri=" + KAKAO_LOGOUT_REDIRECT_URI;
+
+        // 클라이언트에게 리다이렉트 응답을 보냄
+        response.sendRedirect(redirectUrl);
+    }
 
     @GetMapping("/telegram")
-    public String getTelegramSettings(Model model){
-        model.addAttribute("asideMenus",getAsideMenus());
-        model.addAttribute("title","텔레그램 설정");
+    public String getTelegramSettings(Model model) {
+        model.addAttribute("asideMenus", getAsideMenus());
+        model.addAttribute("title", "텔레그램 설정");
         return "setting/telegramSetting";
     }
+
     @PostMapping("/telegram")
-    public String generateTelegramUuid(Model model){
+    public String generateTelegramUuid(Model model) {
 
         GenerateTelegramUuidOut generateTelegramUuidOut = memberBusinessService.generateTelegramUuid(authUtil.getMemberId());
         model.addAttribute("generateTelegramUuidOut", generateTelegramUuidOut);
-        model.addAttribute("asideMenus",getAsideMenus());
+        model.addAttribute("asideMenus", getAsideMenus());
         return "setting/telegramSettingUUidGenerated";
     }
 
 
     @GetMapping("/keyword")
-    public String getKeywordSettings(Model model, RedirectAttributes redirectAttributes){
+    public String getKeywordSettings(Model model, RedirectAttributes redirectAttributes) {
         List<String> mapMemberKeywords = new ArrayList<>();
         try {
-            mapMemberKeywords=keywordBusinessService.getKeywordsByMemberId(authUtil.getMemberId());
-        }catch (BusinessException e){
-            if(e.getErrorCode().equals(MemberErrorCode.TELEGRAM_UUID_NOT_FOUND)){
-                redirectAttributes.addFlashAttribute("alert","먼저 텔레그램 연동을 진행하세요");
+            mapMemberKeywords = keywordBusinessService.getKeywordsByMemberId(authUtil.getMemberId());
+        } catch (BusinessException e) {
+            if (e.getErrorCode().equals(MemberErrorCode.TELEGRAM_UUID_NOT_FOUND)) {
+                redirectAttributes.addFlashAttribute("alert", "먼저 텔레그램 연동을 진행하세요");
                 return "redirect:/api/view/settings/telegram";
-            }else{
+            } else {
                 throw e;
             }
         }
-        model.addAttribute("asideMenus",getAsideMenus());
-        model.addAttribute("keywords",mapMemberKeywords);
-        model.addAttribute("title","키워드 설정");
+        model.addAttribute("asideMenus", getAsideMenus());
+        model.addAttribute("keywords", mapMemberKeywords);
+        model.addAttribute("title", "키워드 설정");
         return "setting/keywordSetting";
     }
 
-    private List<AsideMenu> getAsideMenus(){
-        return new ArrayList<>(List.of(new AsideMenu("keywords","/api/view/settings/keyword"),
-                new AsideMenu("telegram","/api/view/settings/telegram")));
+    private List<AsideMenu> getAsideMenus() {
+        return new ArrayList<>(List.of(new AsideMenu("keywords", "/api/view/settings/keyword"),
+                new AsideMenu("telegram", "/api/view/settings/telegram")));
     }
 }
