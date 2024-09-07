174a032456c56cbe9318dfbb6fb52cc89ce39919
PC <yeonwoo_lee3@tmax.co.kr>
2024-09-07 14:49:10 +0900

    feat:인증실패시 로그인화면으로 이동

--- src/main/java/com/yanoos/global/interceptor/AuthenticationInterceptor.java
+++ src/main/java/com/yanoos/global/interceptor/AuthenticationInterceptor.java
@@ -29,24 +29,29 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
             return true;
         }
         log.info("인증 인터셉터 시작");
-        String jwt = jwtTokenService.getJwtFromRequest(request,TokenType.ACCESS);
-        jwtTokenService.validateToken(jwt, TokenType.ACCESS, response);
-        Long memberId = jwtTokenService.getUserIdFromJwt(jwt);//jwt에서 사용자 id 추출
-        log.info("memberId = {}",memberId);
-        CustomPrincipal principal = new CustomPrincipal(); //사용자의 주요 정보와 권한
-        principal.setMemberId(memberId);
-        UsernamePasswordAuthenticationToken authentication  =
-                new UsernamePasswordAuthenticationToken(principal, null,
-                        Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));//TODO 역할 구현
-        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        try {
+            String jwt = jwtTokenService.getJwtFromRequest(request, TokenType.ACCESS);
+            jwtTokenService.validateToken(jwt, TokenType.ACCESS, response);
+            Long memberId = jwtTokenService.getUserIdFromJwt(jwt);//jwt에서 사용자 id 추출
+            log.info("memberId = {}", memberId);
+            CustomPrincipal principal = new CustomPrincipal(); //사용자의 주요 정보와 권한
+            principal.setMemberId(memberId);
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(principal, null,
+                            Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));//TODO 역할 구현
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
 
-        // 인증 세부 정보 설정
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        // SecurityContext에 인증 정보 저장
-        log.info("인증인터셉터성공");
-        return true;
-
+            // 인증 세부 정보 설정
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            // SecurityContext에 인증 정보 저장
+            log.info("인증인터셉터성공");
+            return true;
+        }catch (Exception e){
+            log.error("인증 인터셉터 실패");
+            response.sendRedirect("/");
+            return false;
+        }
     }
 
     private boolean isExcludedMethod(HttpServletRequest request) {
